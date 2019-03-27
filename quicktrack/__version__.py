0a843affdd38cbf91d15341a9819e92ba4205ad4
justengel <jtengel08@gmail.com>
2019-03-26 20:59:16 -0400

    Initial project creation.

---
+++ quicktrack/__version__.py
@@ -0,0 +1,6 @@
+name = 'django_quicktrack'
+version = '0.0.1'
+description = 'Django app to quickly record when events happen.'
+url = 'http://hg/Webapps/TestDataSystem'
+author = 'Justin Engel'
+author_email = 'jengel@sealandaire.com'
---
+++ quicktrack/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuicktrackConfig(AppConfig):
+    name = 'quicktrack'
